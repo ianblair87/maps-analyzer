8232ac973a7e73e78a5b4bbde2e19b0475c2f06c
Konstantin Amelichev <kostya.amelichev@gmail.com>
2023-12-12 23:54:42 +0100

    triangle demo

--- triangle.py
+++ triangle.py
@@ -4,10 +4,12 @@ import cv2
 
 
 def detect_start(course, erode, dilate, visualize=False):
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (dilate,dilate))
-    course = cv2.dilate(course, kernel)
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (erode,erode))
-    course = cv2.erode(course, kernel)
+    if dilate != 0:
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (dilate,dilate))
+        course = cv2.dilate(course, kernel)
+    if erode != 0:   
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (erode,erode))
+        course = cv2.erode(course, kernel)
     contours,hierarchy = cv2.findContours(course, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     triangles = []
     for cnt in contours:
@@ -16,13 +18,13 @@ def detect_start(course, erode, dilate, visualize=False):
             l = np.linalg.norm(approx[0] - approx[1])
             all_sizes_equal = True
             for i in range(3):
-                if abs(np.linalg.norm(approx[i] - approx[(i + 1) % 3]) - l) / l > 0.1:
+                if abs(np.linalg.norm(approx[i] - approx[(i + 1) % 3]) - l) / l > 0.2:
                     all_sizes_equal = False
             if not all_sizes_equal:
                 continue
             if visualize:
                 res = np.zeros_like(course)
-                res = cv2.drawContours(res, [cnt], -1, 255, 1)
+                res = cv2.drawContours(res, [cnt], -1, 255, 10)
                 plt.imshow(res)
                 plt.show()
             
